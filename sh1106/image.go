e98c4142f2e0db1c19880a318ba9ebb9b6bff2e5
Homin Lee <homin.lee@suapapa.net>
2016-12-02 17:50:53 +0900

    sh1106: add LICENSE

--- sh1106/image.go
+++ sh1106/image.go
@@ -1,3 +1,7 @@
+// Copyright 2016, Homin Lee <homin.lee@suapapa.net>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package sh1106
 
 import "image"
