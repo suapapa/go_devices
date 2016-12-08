e1c66bc5130b7be6b83f4ae3a5b9cbbaa167530b
Homin Lee <homin.lee@suapapa.net>
2016-12-08 16:27:58 +0900

    sh1106: update example

--- sh1106/examples/oled_gopher/main.go
+++ sh1106/examples/oled_gopher/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/png"
 	"os"
 
+	"github.com/suapapa/go_devices/rpi"
 	"github.com/suapapa/go_devices/sh1106"
 	"golang.org/x/exp/io/i2c"
 )
 
 func main() {
-	l, err := sh1106.OpenI2C(&i2c.Devfs{Dev: "/dev/i2c-1"}, 0x3C, nil)
+	l, err := sh1106.OpenI2C(
+		&i2c.Devfs{Dev: "/dev/i2c-1"},
+		&rpi.GpioMem{
+			PinMap: func(n string) (int, error) {
+				switch n {
+				case "RST":
+					return 14, nil
+				}
+				return 0, fmt.Errorf("unknown gpio %s", n)
+			},
+		},
+		0x3C,
+	)
 	if err != nil {
 		panic(err)
 	}
