2c79252b38769848d7981954cee5004a3420f175
Homin Lee <homin.lee@suapapa.net>
2020-02-25 15:36:53 +0900

    edp7in5: gray3: update

--- epd7in5/gray3/color.go
+++ epd7in5/gray3/color.go
@@ -2,19 +2,31 @@ package gray3
 
 import "image/color"
 
-type Gray3 struct {
-	Y uint8
-}
+// Gray3 implements a 3 kinds of gray color.
+type Gray3 uint8
 
-func (c Gray3) RGBA() (r, g, b, a uint32) {
-	y := uint32(c.Y)
-	y |= y << 8
-	return y, y, y, 0xffff
-}
-
-var (
-	Gray3Model color.Model = color.ModelFunc(gray3Model)
+// Possible colors
+const (
+	White Gray3 = iota
+	Gray
+	Black
 )
+
+// RGBA returns either all white, gray or black.
+func (g Gray3) RGBA() (uint32, uint32, uint32, uint32) {
+	switch g {
+	case White:
+		return 0xffff, 0xffff, 0xffff, 0xffff
+	case Gray:
+		return 0xff00, 0xff00, 0xff00, 0xffff
+	case Black:
+		return 0, 0, 0, 0xffff
+	}
+	return 0, 0, 0, 0xffff
+}
+
+// Gray3Model is color model for white, gray or black color.
+var Gray3Model = color.ModelFunc(gray3Model)
 
 func gray3Model(c color.Color) color.Color {
 	if _, ok := c.(Gray3); ok {
@@ -23,5 +35,10 @@ func gray3Model(c color.Color) color.Color {
 	r, g, b, _ := c.RGBA()
 	y := (19595*r + 38470*g + 7471*b + 1<<15) >> 24
 
-	return Gray3{uint8(y)}
+	if y < 64 {
+		return Black
+	} else if y < 192 {
+		return Gray
+	}
+	return White
 }
